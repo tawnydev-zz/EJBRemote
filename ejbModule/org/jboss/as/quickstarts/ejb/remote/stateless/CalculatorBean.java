b866f7f0c01fa7c2f3147f3dea7d7085184c3b34
tawnydev <kevin.matrix@hotmail.fr>
2016-10-16 17:53:04 +0200

    Nouveau Projet
    Session Stateless EJB3 en remote

---
+++ ejbModule/org/jboss/as/quickstarts/ejb/remote/stateless/CalculatorBean.java
@@ -0,0 +1,32 @@
+package org.jboss.as.quickstarts.ejb.remote.stateless;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+@Stateless
+@Remote(RemoteCalculator.class)
+public class CalculatorBean implements RemoteCalculator {
+	static Logger logger = Logger.getLogger(CalculatorBean.class);
+	
+	public void Calculator(){
+    	logger.setLevel(Level.DEBUG);
+    	BasicConfigurator.configure();
+    	logger.info("Appel du Construteur de CalculatorBean");
+	}
+	
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+        logger.info("Action de la méthode add du CalculatorBean avec comme paramètre : "+a+" et "+b);
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		// TODO Auto-generated method stub
+		logger.info("Action de la méthode substract du CalculatorBean avec comme paramètre : "+a+" et "+b);
+		return a - b;
+	}
+}
---
+++ ejbModule/org/jboss/as/quickstarts/ejb/remote/stateless/RemoteCalculator.java
@@ -0,0 +1,8 @@
+package org.jboss.as.quickstarts.ejb.remote.stateless;
+
+public interface RemoteCalculator {
+ 
+    int add(int a, int b);
+ 
+    int subtract(int a, int b);
+}
